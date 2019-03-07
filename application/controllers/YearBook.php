a5832feec0d730dcf35507c4eedb24f2accd5f28
Kisakyamukama <kabalimichael03@gmail.com>
2019-03-07 06:23:53 +0300

    merged but after am not sure of the changes that require committing

--- application/controllers/YearBook.php
+++ application/controllers/YearBook.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class YearBook extends CI_Controller {
 
-	
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
